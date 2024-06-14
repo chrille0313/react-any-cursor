39b8736b2c0c59d87437604075b43c73cf19f64c
Christian <chrille_0313@hotmail.com>
2024-06-14 22:31:14 +0200

    chore: install and configure rollup

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', sourceMap: false }),
+      terser(),
+      postcss({
+        minimize: true
+      })
+    ],
+    external: ['react', 'react-dom']
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: packageJson.types, format: 'es' }],
+    plugins: [dts.default()],
+    external: ['/.css$/']
+  }
+];
