f35ef8383c512562341027ab9167cf7586d35649
Christian <chrille_0313@hotmail.com>
2024-06-15 17:37:04 +0200

    fix: use esm import in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-
-const packageJson = require('./package.json');
+import packageJson from './package.json';
 
 export default [
   {
